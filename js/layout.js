37e1e33e033a8336cbbc3cc03e0285b5512d086b
choi-da-ye <976431zico@gmail.com>
2024-10-29 22:17:58 +0900

    다예 레이아웃 작업중

--- js/layout.js
+++ js/layout.js
@@ -1,12 +1,27 @@
-const handleScroll = () => {
-    const header = document.querySelector('header');
-    scrollTop = window.scrollY || document.documentElement.scrollTop;
+document.addEventListener("DOMContentLoaded", () => {
+    const handleScroll = () => {
+        const header = document.querySelector('header');
+        scrollTop = window.scrollY || document.documentElement.scrollTop;
+    
+        if (scrollTop >= 40) { // 
+            header.classList.add("bw");
+        } else {
+            header.classList.remove("bw");
+        }
+    };
+    
+    let consultClick = () =>{
 
-    if (scrollTop >= 40) { // 
-        header.classList.add("bw");
-    } else {
-        header.classList.remove("bw");
+        const consultBtn = document.querySelector('.consult_btn_wrap .btn');
+        const consultEl = document.querySelector('.consult_btn_wrap');
+        consultBtn.addEventListener('click', () => {
+            consultEl.classList.toggle('active');
+        });
     }
-};
+    setTimeout(consultClick, 200);
 
-window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
+
+})
+
+
