5c0a11901faff31d7b4aff0feae0be430f90feb9
choi-da-ye <976431zico@gmail.com>
2024-11-02 00:22:44 +0900

    d

--- js/main.js
+++ js/main.js
@@ -13,13 +13,48 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   
     gsap.to(".sec04 .inner", {
-      x: "-100%",
+      x: `${window.innerWidth - 5000}px`,
       scrollTrigger: {
         trigger: ".sec04",
         pin:true,
         start: "top top",
-        scrub:2,
+        end:"500% bottom",
+        scrub:5,
         toggleActions: "play none none none",
+        // markers:true,
+      },
+
+    });
+
+    let sec05_conts = document.querySelectorAll('.sec05 .cont');
+    sec05_conts.forEach((cont) => {
+      let img = cont.querySelectorAll('.img_wrap img');
+      let li = cont.querySelectorAll('ul li');
+    
+      li.forEach((li, idx) => {
+        li.addEventListener('mouseenter', () => {
+          img.forEach((img) => {
+            img.classList.remove('active');
+          });
+          img[idx].classList.add('active');
+        });
+      });
+    });
+
+    var sec06Swiper = new Swiper(".sec06 .swiper", {
+      slidesPerView:2.5,
+      spaceBetween:16,
+    });
+
+    var sec07Swiper = new Swiper(".sec07 .swiper", {
+      // slidesPerView:'auto',
+      slidesPerView:3.9,
+      spaceBetween:124,
+      slidesOffsetBefore:263,
+      slidesOffsetAfter:263,
+      navigation: {
+        nextEl: ".sec07 .next",
+        prevEl: ".sec07 .prev",
       },
     });
 
