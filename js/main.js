4d70250932ad4e02aae25c0d402ca2d272a536e5
choi-da-ye <976431zico@gmail.com>
2024-11-02 22:30:48 +0900

    다예

--- js/main.js
+++ js/main.js
@@ -41,17 +41,72 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
 
+    let sec05Moconts = document.querySelectorAll('.sec05_mo .cont');
+    sec05Moconts.forEach((cont) => {
+      let imgs = cont.querySelectorAll('.img_wrap img');
+      let li = cont.querySelectorAll('ul li');
+    
+      li.forEach((li, idx,arr) => {
+        li.addEventListener('click', () => {
+          imgs.forEach((img) => {
+            img.classList.remove('active');
+          });
+          arr.forEach((el) => {
+            el.classList.remove('active');
+          });
+          li.classList.add('active');
+          imgs[idx].classList.add('active');
+        });
+      });
+    });
+
     var sec06Swiper = new Swiper(".sec06 .swiper", {
-      slidesPerView:2.5,
-      spaceBetween:16,
+      centeredSlides: true,
+      spaceBetween:9,
+      initialSlide: 2,
+      breakpoints: {
+        880: {
+          centeredSlides: false,
+          initialSlide: 0,
+          slidesPerView:1.5,
+          spaceBetween:16,
+        },
+        1400: {
+          centeredSlides: false,
+          initialSlide: 0,
+          slidesPerView:2.5,
+          spaceBetween:16,
+        }
+      },
+ 
     });
 
     var sec07Swiper = new Swiper(".sec07 .swiper", {
       // slidesPerView:'auto',
-      slidesPerView:3.9,
-      spaceBetween:124,
-      slidesOffsetBefore:263,
-      slidesOffsetAfter:263,
+      slidesPerView:1.4,
+      spaceBetween:16,
+      slidesOffsetBefore:34,
+      slidesOffsetAfter:34,
+      breakpoints: {
+        880: {
+          slidesPerView:3.9,
+          spaceBetween:124,
+          slidesOffsetBefore:263,
+          slidesOffsetAfter:263,
+        }
+      },
+      navigation: {
+        nextEl: ".sec07 .next",
+        prevEl: ".sec07 .prev",
+      },
+    });
+
+    var sec04MoSwiper = new Swiper(".sec04_mo .swiper", {
+      // slidesPerView:'auto',
+      slidesPerView:1.17,
+      spaceBetween:12,
+      slidesOffsetBefore:16,
+      slidesOffsetAfter:16,
       navigation: {
         nextEl: ".sec07 .next",
         prevEl: ".sec07 .prev",
