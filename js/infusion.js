05db95ca37c59ab9d73652ae399b6a44e536fdaf
choi-da-ye <976431zico@gmail.com>
2024-11-09 17:45:00 +0900

    다에

---
+++ js/infusion.js
@@ -0,0 +1,12 @@
+var sec11Swiper = new Swiper(".sec11 .swiper", {
+    slidesPerView:'1',
+    slidesPerView:1,
+    spaceBetween:16,
+    // slidesOffsetBefore:16,
+    // slidesOffsetAfter:16,
+    navigation: {
+      nextEl: ".sec11 .next",
+      prevEl: ".sec11 .prev",
+    },
+    initialSlide:1
+  });
