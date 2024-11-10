35291a3eb7dac19223e9b22477ae859fcc93dafa
choi-da-ye <976431zico@gmail.com>
2024-11-10 15:53:54 +0900

    dd

--- js/infusion.js
+++ js/infusion.js
@@ -1,12 +1,31 @@
-var sec11Swiper = new Swiper(".sec11 .swiper", {
-    slidesPerView:'1',
+var sec07Swiper = new Swiper(".sec07 .swiper", {
+    slidesPerView:'2',
     slidesPerView:1,
-    spaceBetween:16,
+    spaceBetween:10,
+    centerMode:true,
     // slidesOffsetBefore:16,
     // slidesOffsetAfter:16,
     navigation: {
-      nextEl: ".sec11 .next",
-      prevEl: ".sec11 .prev",
+        nextEl: ".sec07 .nb",
+        prevEl: ".sec07 .pb",
     },
     initialSlide:1
-  });
+});
+
+var sec11Swiper = new Swiper(".sec11 .swiper", {
+    slidesPerView:'1',
+    slidesPerView:1,
+    spaceBetween:40,
+    breakpoints : {
+        880 : {
+            spaceBetween:16,
+        }
+    },
+    // slidesOffsetBefore:16,
+    // slidesOffsetAfter:16,
+    navigation: {
+        nextEl: ".sec11 .next",
+        prevEl: ".sec11 .prev",
+    },
+    initialSlide:1
+});
